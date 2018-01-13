ee46e5da8812707754cb28dff375d1bd357b9dc4
burak <tknbrk@gmail.com>
2018-01-13 14:05:15 +0100

    Add data loader

--- WiSe17-18/preprocessing/load_data.py
+++ WiSe17-18/preprocessing/load_data.py
@@ -3,9 +3,8 @@
 import tensorflow as tf
 import os
 
-dir_path = os.path.dirname(os.path.abspath(__file__))
-print(dir_path)
-filename = dir_path + "/pos/en-train.txt"
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(ROOT_DIR, "../pos/en-train.txt")
 
 tokens = tf.placeholder(tf.int32, name='tokens')
 pos_tags = tf.placeholder(tf.string, name='pos_tags')
@@ -15,12 +14,12 @@ printerop = tf.Print(total, [tokens, pos_tags], name='printer')
 
 with tf.Session() as sess:
     sess.run( tf.global_variables_initializer())
-    with open(filename) as inf:
+    with open(DATA_DIR) as inf:
         for line in inf:
-            # Read data, using python, into our features
-            token, pos_tag = line.strip().split("\t")
-            token = str(token)
-            pos_tag = str(pos_tag)
-            # Run the Print ob
-            total = sess.run(printerop, feed_dict={pos_tags: pos_tag, tokens: token})
-            print(token, total)
+            if line.strip():
+                # Read data, using python, into our features
+                token, pos_tag = line.strip().split("\t")
+                token = str(token)
+                pos_tag = str(pos_tag)
+                # Run the Print ob
+                print(token, total)
