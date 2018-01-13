a4ba4549907633495f0ec6496aea9d91cdd1b38e
burak <tknbrk@gmail.com>
2018-01-14 00:30:04 +0100

    Create word embedding dictionary

--- WiSe17-18/preprocessing/load_data.py
+++ WiSe17-18/preprocessing/load_data.py
@@ -1,31 +1,34 @@
 # -*- coding: utf-8 -*-
 
-import tensorflow as tf
 import os
-
-
 import numpy as np
-filename = 'glove.6B.50d.txt'
+import tensorflow as tf
+
+
+filename = '../glove.6B.50d.txt'
+
 def loadGloVe(filename):
-    vocab = []
-    embd = []
+    embedding_dict = dict()
     file = open(filename,'r')
     for line in file.readlines():
-        row = line.strip().split(' ')
-        vocab.append(row[0])
-        embd.append(row[1:])
+        row = line.split(" ")
+        embedding_dict[row[0]] = [float(i) for i in row[1:]]
+    
     print('Loaded GloVe!')
     file.close()
-    return vocab,embd
-vocab,embd = loadGloVe(filename)
-vocab_size = len(vocab)
-embedding_dim = len(embd[0])
-embedding = np.asarray(embd)
+    return embedding_dict
 
-W = tf.Variable(tf.constant(0.0, shape=[vocab_size, embedding_dim]),
-                trainable=False, name="W")
+# Load Glove pretrained vocabulary
+embd = loadGloVe(filename)
+vocab_size = len(embd)
+embedding_dim = len(list((embd.values()))[0])
+embedding = np.asarray(embd.values)
+
+W = tf.Variable(tf.constant(0.0, shape=[vocab_size, embedding_dim]), trainable=False, name="W")
 embedding_placeholder = tf.placeholder(tf.float32, [vocab_size, embedding_dim])
 embedding_init = W.assign(embedding_placeholder)
+
+print(embd['the'])
 
 init_op = tf.global_variables_initializer()
 # init_op = tf.initialize_all_variables()
