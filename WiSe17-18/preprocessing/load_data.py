2927579965473bb09bcb73875db53d5985fe1c41
burak <tknbrk@gmail.com>
2018-01-13 16:28:48 +0100

    Add missing files

--- WiSe17-18/preprocessing/load_data.py
+++ WiSe17-18/preprocessing/load_data.py
@@ -10,16 +10,12 @@ tokens = tf.placeholder(tf.int32, name='tokens')
 pos_tags = tf.placeholder(tf.string, name='pos_tags')
 total = tf.reduce_sum(tokens, name='total')
 
-printerop = tf.Print(total, [tokens, pos_tags], name='printer')
-
 with tf.Session() as sess:
     sess.run( tf.global_variables_initializer())
     with open(DATA_DIR) as inf:
         for line in inf:
             if line.strip():
-                # Read data, using python, into our features
                 token, pos_tag = line.strip().split("\t")
                 token = str(token)
                 pos_tag = str(pos_tag)
-                # Run the Print ob
                 print(token, pos_tag)
---
+++ WiSe17-18/rnn_models/ner_rnn.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Team LSD-M
+# Purpose: RNN implementation for NER tagger via TensorFlow
+
+import tensorflow as tf
