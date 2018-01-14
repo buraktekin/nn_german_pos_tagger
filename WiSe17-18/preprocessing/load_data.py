80fb46249260c3b7fe34091ce63ee11c9eb0478a
burak <tknbrk@gmail.com>
2018-01-14 01:00:43 +0100

    modify to OOP structure

--- WiSe17-18/preprocessing/load_data.py
+++ WiSe17-18/preprocessing/load_data.py
@@ -5,58 +5,41 @@ import numpy as np
 import tensorflow as tf
 
 
-filename = '../glove.6B.50d.txt'
+class Preprocessing:
 
-def loadGloVe(filename):
-    embedding_dict = dict()
-    file = open(filename,'r')
-    for line in file.readlines():
-        row = line.split(" ")
-        embedding_dict[row[0]] = [float(i) for i in row[1:]]
-    
-    print('Loaded GloVe!')
-    file.close()
-    return embedding_dict
+    def __init__(self):
+        filename = '../glove.6B.50d.txt'
+        # Load Glove pretrained vocabulary
+        embedding_vocabulary = self.loadGloVe(filename)
+        vocab_size = len(embedding_vocabulary)
+        embedding_dim = len(list((embedding_vocabulary.values()))[0])
+        embedding = np.asarray(embedding_vocabulary.values())
 
-# Load Glove pretrained vocabulary
-embd = loadGloVe(filename)
-vocab_size = len(embd)
-embedding_dim = len(list((embd.values()))[0])
-embedding = np.asarray(embd.values)
+        W = tf.Variable(tf.constant(0.0, shape=[vocab_size, embedding_dim]), trainable=False, name="W")
+        embedding_placeholder = tf.placeholder(tf.float32, [vocab_size, embedding_dim])
+        embedding_init = W.assign(embedding_placeholder)
+        
+        print(embedding_vocabulary['the'])
 
-W = tf.Variable(tf.constant(0.0, shape=[vocab_size, embedding_dim]), trainable=False, name="W")
-embedding_placeholder = tf.placeholder(tf.float32, [vocab_size, embedding_dim])
-embedding_init = W.assign(embedding_placeholder)
-
-print(embd['the'])
-
-init_op = tf.global_variables_initializer()
-# init_op = tf.initialize_all_variables()
-with tf.Session() as sess:
-    sess.run(init_op)
-    print(sess.run(W))
-    # init.run()
-    # sess.run(embedding_init, feed_dict={embedding_placeholder: embedding})
-    # tf.Print(W[0], vocab)
-    # for i in vocab[:10]:
-    #     print(i)
-    # print(embedding[0])
-
-# from tensorflow.contrib import learn
-# #init vocab processor
-# vocab_processor = learn.preprocessing.VocabularyProcessor(max_document_length)
-# #fit the vocab from glove
-# pretrain = vocab_processor.fit(vocab)
-# #transform inputs
-# x = np.array(list(vocab_processor.transform(your_raw_input)))
+        init_op = tf.global_variables_initializer()
+        # init_op = tf.initialize_all_variables()
+        with tf.Session() as sess:
+            sess.run(init_op)
+            print(sess.run(W))
+        
+    def loadGloVe(self, filename):
+        embedding_dict = dict()
+        file = open(filename,'r')
+        for line in file.readlines():
+            row = line.split(" ")
+            embedding_dict[row[0]] = [float(i) for i in row[1:]]
+        
+        print('Loaded GloVe!')
+        file.close()
+        return embedding_dict
 
 
-
-
-
-
-
-
+p = Preprocessing()
 
 
 # ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
