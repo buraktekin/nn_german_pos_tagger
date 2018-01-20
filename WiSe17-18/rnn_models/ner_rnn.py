a47f5edce3d079c5aad4b6372ab43a3f3e1bc67f
burak <tknbrk@gmail.com>
2018-01-20 09:03:22 +0100

    Modify word embedding

--- WiSe17-18/rnn_models/ner_rnn.py
+++ WiSe17-18/rnn_models/ner_rnn.py
@@ -3,3 +3,10 @@
 # Purpose: RNN implementation for NER tagger via TensorFlow
 
 import tensorflow as tf
+import sys
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(ROOT_DIR + '/../')
+from preprocessing.load_data_glove import Preprocessing
+p = Preprocessing()
